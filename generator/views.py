03b2334aae1707f1a64807d7437ec1ab51a763d0
Miguel Corkhill <moogwol@msn.com>
2021-10-01 15:17:57 +0200

    First commit

---
+++ generator/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render # renders templates
+from django.http import HttpResponse
+
+import random
+
+# Create your views here.
+def home(request):
+    return render(request, 'generator/home.html')
+
+def password(request):
+
+    user_password = ''
+    characters = list('abcdefghijklmnopqrstuvxyz')
+    length = int(request.GET.get('length'))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special-characters'):
+        characters.extend(list('!£$%^&*()@~#?|'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    for i in range(length):
+        user_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': user_password})
+
+def about(request):
+    return render(request, 'generator/about.html')
